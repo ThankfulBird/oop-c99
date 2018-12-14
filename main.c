e6d0cca942d2f61cec1202bdb7e8f3b1518884c3
bera <z.bera97@gmail.com>
2018-12-14 03:01:14 +0300

    Constructor and to_string function has added to the object.h.

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
-#include <stdio.h>
+#include "object.h"
+#define my_object this
 
 int main() {
-    printf("Hello, World!\n");
+    Object my_object = _Object();
+    printf("%s\n", my_object->to_string(string, this));
     return 0;
 }
--- object.h
+++ object.h
@@ -5,6 +5,44 @@
 #ifndef OOP_C99_OBJECT_H
 #define OOP_C99_OBJECT_H
 
+#include <stdio.h>
+#include <stdlib.h>
 
+typedef enum
+{
+    FALSE, TRUE
+} Boolean;
+typedef struct Object _Object_;
+typedef _Object_ * Object;
+
+struct Object
+{
+    char * (*to_string)(char string[], Object object);
+};
+
+char string[50];
+
+Object _Object();
+char * to_string(char string[], Object object);
+
+Object _Object()
+{
+    Object new_object = (Object)malloc(sizeof(_Object_));
+    if (new_object != NULL)
+    {
+        new_object->to_string = to_string;
+        return new_object;
+    }
+    else
+    {
+        _Object();
+    }
+}
+
+char * to_string(char string[], Object object)
+{
+    sprintf(string, "[Object@%p]", object);
+    return string;
+}
 
 #endif //OOP_C99_OBJECT_H
